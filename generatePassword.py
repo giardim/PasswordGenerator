ad67f718a078e702f2e0d517e6726e32980a6cdb
Michael Giardina <mgiardina02@gmail.com>
2022-10-30 19:45:16 -0400

    Added documentation

--- generatePassword.py
+++ generatePassword.py
@@ -1,7 +1,16 @@
+##################################
+#A program that generates a random password
+#   and saves it in a file
+#Written by Michael Giardina
+#Python 3.9.1
+#30 OCT 2022
+##################################
+
 import random
 
 class generatePassword:
     def __init__(self, pLength, specialChar, num, cap, low):
+        #set up variables
         specialList = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
         numList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
         capList = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -18,23 +27,43 @@ class generatePassword:
         password = ""
         f = open("password.txt", "a")
 
+        #Make sure atleast one perameter is passed with a valid number
         if (not specialChar and not num  and not cap and not low):
             pLength = 0
             f.write("You need to select atleast one option")
 
+        #While the password length the user requested is 
+        #   greater than 0
         while (pLength > 0):
+            #get a random integer, this will determine
+            #   what list to take a character from
             randInt = random.randint(0, 3)
+            #if the random integer is 0
             if (randInt == 0 and specialChar):
+                #take a random character from the special list 
+                #   and decrement the password length
                 password += random.choice(specialList)
                 pLength -= 1
+            #if the random integer is 1
             elif (randInt == 1 and num):
+                #take a random character from the number list 
+                #   and decrement the password length
                 password += random.choice(numList)
                 pLength -= 1
+            #if the random integer is 2
             elif (randInt == 2 and cap):
+                #take a random character from the special list 
+                #   and decrement the capital letters list
                 password += random.choice(capList)
                 pLength -= 1
+            #if the random integer is 3
             elif (randInt == 3 and low):
+                #take a random character from the lowercase letters list 
+                #   and decrement the password length
                 password += random.choice(lowList)
                 pLength -= 1
+        #add a new line escape sequence to the end of the password
         password += "\n"
+        
+        #save the password to the file
         f.write(password)
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from tkinter import *
 from generatePassword import *
 
 def main():
+    #set up variables
     root = Tk()
     root.geometry("500x500")
     root.title("Password Generator")
@@ -19,33 +20,44 @@ def main():
     cap = IntVar()
     lower = IntVar()
 
+    #Put a title at the top of the window
     title = Label(text = "Password Generator", font = 15)
     title.place(x = 150, y = 5)
 
-    explainSlider = Label(text = "Password Length?", font = 10)
+    #explain what the slider does
+    explainSlider = Label(text = "Password Length", font = 10)
     explainSlider.place(x = 50, y = 75)
 
+    #puts the slider on the screen
     psswrdLength = Scale(root, from_= 1, to = 32, orient = HORIZONTAL, sliderlength = 15, length = 300, bg = "black", fg = "white")
     psswrdLength.place(x = 50, y = 100)
 
-    askSpecialChar = Checkbutton(root, text = "Special Characters?", variable = spChar, font = 10)
+    #ask the user if they want special characters in their password
+    askSpecialChar = Checkbutton(root, text = "Special Characters", variable = spChar, font = 10)
     askSpecialChar.place(x = 50, y = 150)
 
+    #ask the user if they want numbers in their password
     askNum = Checkbutton(root, text = "Numbers?", variable = num, font = 10)
     askNum.place(x = 50, y = 200)
 
+    #ask the user if they want capital letters in their password
     askCapital = Checkbutton(root, text = "Capital Letters?", variable = cap, font = 10)
     askCapital.place(x = 50, y = 250)
 
+    #ask the user if they want lowercase letters in their password
     askLower = Checkbutton(root, text = "Lowercase?", variable = lower, font = 10)
     askLower.place(x = 50, y = 300)
 
+    #place a button that allows the user to quit the program
     quit = Button(root, text = "Quit", command = lambda:(root.destroy()), padx = 10, pady = 10, font = 10, bg = "black", fg = "orange")
     quit.place(x = 400, y = 400)
 
+    #place a button that generates the password when clicked
     createPword = Button(root, text = "Generate Password", command = lambda:(generatePassword(psswrdLength.get(), spChar.get(), num.get(), cap.get(), lower.get())), padx = 10, pady = 10, font = 10, bg = "black", fg = "orange")
     createPword.place (x = 50, y = 400)
 
+    #keep the program running
     root.mainloop()
 
+#call main function
 main()
